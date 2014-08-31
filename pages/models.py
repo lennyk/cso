c30a23ebbe81ca79eaf9efb4dad08012e0ae5e62
Lenny Koepsell <lennyk@lennyk.com>
2014-08-30 23:50:27 -0700

    allow longer Date tile & description

--- pages/models.py
+++ pages/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Date(models.Model):
     date = models.DateField()
     time = models.TimeField(null=True, blank=True)
-    title = models.CharField(max_length=30)
-    description = models.CharField(max_length=160)
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=500)
     information = models.TextField()
     is_active = models.BooleanField(default=True)
