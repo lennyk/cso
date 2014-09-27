f0d0a753d7abcbba18f917485f580d4379b7a0da
Lenny Koepsell <lennyk@lennyk.com>
2014-09-27 15:35:29 -0700

    improve logout link to automatically submit csrf protected form with one click

--- cso/static/base.js
+++ cso/static/base.js
@@ -17,3 +17,9 @@ $('#login-buttons a').click(function() {
     $('#login-buttons').hide();
     $('#login-spinner').show();
 });
+
+// submit the form to logout
+$('#logout-link').click(function() {
+    $(this).parents('form').submit();
+    return false;
+});
