24d5306b132cd3fa7ddc032d6a6b437da6e86848
Lenny Koepsell <lennyk@lennyk.com>
2014-12-15 14:09:10 -0800

    remove fields from Dates, add Dates to fixture
    information from removed fields in Dates has been moved into static Event Information page

--- events/models.py
+++ events/models.py
@@ -7,8 +7,6 @@ class Date(models.Model):
     date = models.DateField()
     time = models.TimeField(null=True, blank=True)
     title = models.CharField(max_length=120)
-    description = models.CharField(max_length=500)
-    information = models.TextField()
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
