b65544faf0e62c8a2bd83acd80630b659a5acfc5
Lenny Koepsell <lennyk@lennyk.com>
2015-03-04 16:17:31 -0800

    fix ticket countdown. wasn't properly calculating dates and times.

--- cso/views.py
+++ cso/views.py
@@ -13,22 +13,28 @@ def home_page(request):
     now = datetime.now()
     ticket_college_presale = {
         # hardcoded id from fixture
-        'date': Date.objects.get(id=1).date
+        'date': datetime.combine(
+            Date.objects.get(id=1).date,
+            Date.objects.get(id=1).time
+        )
     }
-    ticket_college_presale['days_until'] = (ticket_college_presale.get('date') - now.date()).days
+    ticket_college_presale['days_until'] = (ticket_college_presale.get('date').date() - now.date()).days
 
     ticket_public_sale = {
         # hardcoded id from fixture
-        'date': Date.objects.get(id=2).date
+        'date': datetime.combine(
+            Date.objects.get(id=2).date,
+            Date.objects.get(id=2).time
+        )
     }
-    ticket_public_sale['days_until'] = (ticket_public_sale.get('date') - now.date()).days
+    ticket_public_sale['days_until'] = (ticket_public_sale.get('date').date() - now.date()).days
 
     return render(request, 'cso/home.html', {'user': request.user,
                                              'participations': participations,
                                              'active_dates': active_dates,
                                              'ticket_college_presale': ticket_college_presale,
                                              'ticket_public_sale': ticket_public_sale,
-                                             'now': now.date()
+                                             'now': now
                                              })
 
 
