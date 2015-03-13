d7c3e7fab22a783b7327426e6a118a867aa2aa7b
Lenny Koepsell <lennyk@lennyk.com>
2015-03-12 19:45:43 -0700

    improve payment processing form UX with button message and animation

--- cso/static/payment-form.js
+++ cso/static/payment-form.js
@@ -1,29 +1,27 @@
 $(document).ready(function () {
+    var $form = $('#cso-ticket-form');
     var $name = $('#cc-name');
     var $number = $('#cc-number');
     var $expiration = $('#cc-exp');
     var $cvc = $('#cc-cvc');
+    var $submit = $('#cc-submit');
     var $icons = $('#cc-icons');
 
-    $('#cso-ticket-form').submit(function (event) {
-        var $form = $(this);
-        var $submit = $form.find('[type="submit"]');
-        var submitInitialText = $submit.val();
-
-        $submit.attr('disabled', true).val('Processing...');
+    $form.submit(function (event) {
         event.preventDefault();
 
         // reset errors
         $form.find('.has-error').removeClass('has-error');
 
-
         if ($.trim($name.val()).length == 0) {
             // client side validation
-            $name.closest('.form-group').addClass('has-error');
+            $name.focus().closest('.form-group').addClass('has-error');
         } else {
-            // post to server
-            var expiration = $expiration.payment('cardExpiryVal');
+            // disable the submit button before contacting Stripe
+            $submit.attr('disabled', true);
 
+            // post to Stripe
+            var expiration = $expiration.payment('cardExpiryVal');
             Stripe.card.createToken(
                 {
                     name: $name.val(),
@@ -33,31 +31,33 @@ $(document).ready(function () {
                     exp_year: (expiration.year || 0)
                 },
                 function (status, response) {
-
                     if (response.error) {
-                        //$form.find('.stripe-errors').text(response.error.message).show();
-                        $submit.attr('disabled', false).val(submitInitialText);
+                        // errors from Stripe
+                        $submit.attr('disabled', false);
                         switch (response.error.param) {
                             case 'number':
-                                $number.closest('.form-group').addClass('has-error');
+                                $number.focus().closest('.form-group').addClass('has-error');
                                 break;
                             case 'exp_month':
                             case 'exp_year':
-                                $expiration.closest('.form-group').addClass('has-error');
+                                $expiration.focus().closest('.form-group').addClass('has-error');
                                 break;
                             case 'cvc':
-                                $cvc.closest('.form-group').addClass('has-error');
+                                $cvc.focus().closest('.form-group').addClass('has-error');
                                 break;
                         }
                     } else {
+                        // success from Stripe, posting to CSO
+                        $submit.text('Processing ').append($('<i class="fa fa-spinner fa-spin"></i>'));
                         var token = response.id;
                         $form.append($('<input type="hidden" name="stripe_token" />').val(token));
                         $form.get(0).submit();
                     }
-
                 }
             );
         }
+        // Prevent the form from submitting with the default action
+        return false;
     });
 
     (function () {
@@ -65,7 +65,7 @@ $(document).ready(function () {
         $expiration.payment('formatCardExpiry');
         $cvc.payment('formatCardCVC');
 
-        $number.bind('input', function(){
+        $number.bind('input', function () {
             $icons.find('.fa').removeClass('text-primary');
             var type = $.payment.cardType($(this).val());
             if (type == 'visa') {
